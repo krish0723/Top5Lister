7761b3923c8c20c010a2152940fe5976f3b0e39d
krish <krishna.chetan@stonybrook.edu>
2022-02-04 19:34:29 -0500

    build in server

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ app.use(cors({
 }))
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.static('build'));
 
 // SETUP OUR OWN ROUTERS AS MIDDLEWARE
 const authRouter = require('./routes/auth-router')
